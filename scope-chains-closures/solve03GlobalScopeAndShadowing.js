a5a0d57b4e190cb8aca104e10e6e475006ce236c
excalliburbd <excalliburbd@gmail.com>
2016-03-16 23:38:47 +0600

    got-it!

---
+++ scope-chains-closures/solve03GlobalScopeAndShadowing.js
@@ -0,0 +1,11 @@
+function foo() {
+
+	var bar;
+
+	quux = 0;
+
+	function zip() {
+
+		var quux = 1;
+	}
+}
---
+++ scope-chains-closures/solve04Clouser.js
@@ -0,0 +1,14 @@
+function foo() {
+
+	var bar;
+
+	quux = 0;
+
+	function zip() {
+
+		var quux = 1;
+		bar = true;
+	}
+
+	return zip;
+}
